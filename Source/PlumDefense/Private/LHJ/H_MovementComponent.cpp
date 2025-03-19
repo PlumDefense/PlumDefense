5242a223e613339ab6bdd33066c07c189f2068b7
LHcom <55868914+LHcom@users.noreply.github.com>
2025-03-19 19:50:55 +0900

    - 이동 기능

--- Source/PlumDefense/Private/LHJ/H_MovementComponent.cpp
+++ Source/PlumDefense/Private/LHJ/H_MovementComponent.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "LHJ/H_User.h"
-
 #include "LHJ/H_MovementComponent.h"
+
+#include "LHJ/H_User.h"
 
 #include "EnhancedInputComponent.h"
 #include "InputActionValue.h"
@@ -15,11 +15,13 @@ UH_MovementComponent::UH_MovementComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 	bWantsInitializeComponent = true;
-	
-	ConstructorHelpers::FObjectFinder<UInputAction> tmpMove(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Move.IA_Move'"));
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpMove(
+		TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Move.IA_Move'"));
 	if (tmpMove.Succeeded())
 		IA_Move = tmpMove.Object;
-	ConstructorHelpers::FObjectFinder<UInputAction> tmpLook(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Look.IA_Look'"));
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpLook(
+		TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Look.IA_Look'"));
 	if (tmpLook.Succeeded())
 		IA_Mouse = tmpLook.Object;
 }
@@ -31,7 +33,6 @@ void UH_MovementComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
 }
 
 void UH_MovementComponent::InitializeComponent()
@@ -47,22 +48,26 @@ void UH_MovementComponent::InitializeComponent()
 	}
 }
 
-void UH_MovementComponent::SetupInputBinding(class UEnhancedInputComponent* input)
+void UH_MovementComponent::SetupInputBinding(UEnhancedInputComponent* input)
 {
+	input->BindAction(IA_Move, ETriggerEvent::Triggered, this, &UH_MovementComponent::F_Move);
 	input->BindAction(IA_Mouse, ETriggerEvent::Triggered, this, &UH_MovementComponent::F_Turn);
 }
 
 
 // Called every frame
-void UH_MovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UH_MovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                         FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
 void UH_MovementComponent::F_Move(const struct FInputActionValue& Value)
 {
+	FVector2D v = Value.Get<FVector2D>();
+	
+	FVector direction(v.X, v.Y, 0);
+	user->AddMovementInput(user->GetTransform().TransformVector(direction));
 }
 
 void UH_MovementComponent::F_Turn(const struct FInputActionValue& Value)
